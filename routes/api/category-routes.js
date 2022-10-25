2ee2fbf05801e1bfd3bb5a3293c413e13e1065b7
night <yseddiq@gmail.com>
2022-10-25 06:11:24 -0400

    working on Get By Id Route for Category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -28,6 +28,12 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where:{
+      id: req.params.id
+    },
+    include:
+  })
 });
 
 router.post('/', (req, res) => {
