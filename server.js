27b2a0df9050677e6e2ec873014e898f094791f6
night <yseddiq@gmail.com>
2022-10-25 02:49:26 -0400

    added started code

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const sequelize= require('./config/connection')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
 //tun sever on.
 sequelize.sync({force:false}).then(()=>{
     app.listen(PORT, ()=> console.log('server connected.'))
