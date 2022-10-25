90ce53746dce066f140ac6e40b4b07355be9b21d
night <yseddiq@gmail.com>
2022-10-25 03:57:13 -0400

    made progress on prodcut tag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,18 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id:{
+        types: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    product_id:{
+        types: DataTypes.INTEGER,
+        references:{
+            
+        }
+    }
   },
   {
     sequelize,
