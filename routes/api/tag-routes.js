1a33c4fc994d9f282d49cbc479cded4d9ae784e3
night <yseddiq@gmail.com>
2022-10-26 02:18:52 -0400

    got Tag delete routes to work

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -55,6 +55,12 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where:{
+      id: req.params.id
+    }
+  })
+  .then(dbTagData=> res.json(dbTagData));
 });
 
 module.exports = router;
