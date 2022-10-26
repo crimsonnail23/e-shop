707d233f7fddfb0921b92b43909c75af57c16a72
night <yseddiq@gmail.com>
2022-10-26 03:25:11 -0400

    got Category POST route to work

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,6 +48,10 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(dbCategoryData=>res.json(dbCategoryData))
 });
 
 router.put('/:id', (req, res) => {
